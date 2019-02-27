44f38a5ee4ddfccfc152461f073e86d35e7bf1a7
Eiosx <eiosxenzo@gmail.com>
2019-02-26 22:04:32 -0500

    In the process of setting up component paths and file structure.

---
+++ front-end-project/src/components/Navigation/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <div>
+            <h1>Lambda Notes</h1>
+            <nav>
+                <button>View Your Notes</button>
+                <button>+Create New Notes</button>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;
---
+++ front-end-project/src/components/Note/Note.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Note.css';
+
+const Note = props => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
